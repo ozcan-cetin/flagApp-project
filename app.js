9a6868dbdf3e3eb3e14db75679f3b3a4ab07b772
özcan çetin <cetin445@gmail.com>
2022-06-22 23:15:10 +0300

    async await defined

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+//* ASYNC AWAIT SECTION *//
+
 const flagApp = async (name)=>{
     const url = `https://restcountries.com/v3.1/name/${name}`;
 
     try {
         const res = await fetch(url);
+        if(!res.ok){
+            renderError('There is a proplem ${res.status}')
+            throw new Error
+        }
+        const data = await res.json();
+        renderCountry(data[0]);
     } catch (error) {
         console.log(error);
     }
+};
+
+//* FUNCTIONS *//
